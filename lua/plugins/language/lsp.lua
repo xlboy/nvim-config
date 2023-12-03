7de6dcc887277bfff27577bc90fd617970016efd
xlboy <xlboy88888@foxmail.com>
2023-12-03 14:08:09 +0800

    1

--- lua/plugins/language/lsp.lua
+++ lua/plugins/language/lsp.lua
@@ -19,9 +19,8 @@ return {
       end
 
       -- Set up keymaps
-      vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show diagnostics of current line" })
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Move to previous diagnostic" })
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Move to next diagnostic" })
+      -- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Move to previous diagnostic" })
+      -- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Move to next diagnostic" })
 
       -- Add additional capabilities supported by nvim-cmp
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -35,16 +34,15 @@ return {
           return { noremap = true, silent = true, buffer = bufnr, desc = desc }
         end
 
-        vim.keymap.set("n", "gh", vim.lsp.buf.hover, buf_opts("Show signature of current symbol"))
+        -- vim.keymap.set("n", "gh", vim.lsp.buf.hover, buf_opts("Show signature of current symbol"))
         vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, buf_opts("Go to definiton"))
-        vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, buf_opts("Go to implementation"))
-        vim.keymap.set("n", "gr", telescope_builtin.lsp_references, buf_opts("Go to reference"))
-        vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, buf_opts("Rename current symbol"))
-        vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action, buf_opts("Run code action"))
+        -- vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, buf_opts("Go to implementation"))
+        -- vim.keymap.set("n", "gr", telescope_builtin.lsp_references, buf_opts("Go to reference"))
+        -- vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, buf_opts("Rename current symbol"))
+        -- vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action, buf_opts("Run code action"))
         vim.keymap.set("n", "<Leader>cf", function()
           vim.lsp.buf.format({ async = true })
         end, buf_opts("Format current file"))
-
       end
 
       require("mason-lspconfig").setup_handlers({
@@ -69,8 +67,40 @@ return {
       ensure_installed = {
         "lua_ls",
         "clangd",
-        "neocmake"
+        "neocmake",
       },
+    },
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "BufEnter",
+    opts = {
+      symbol_in_winbar = {
+        enable = false,
+      },
+      -- lightbulb = {
+      --   enable = false,
+      -- },
+      finder = {
+        left_width = 0.2,
+      },
+      rename = {
+        keys = {
+          quit = { "q", "<ESC>" },
+        },
+      },
+    },
+    keys = {
+      { "<leader>la", ":Lspsaga code_action<CR>", desc = "Show code actions", mode = "n" },
+      { "<leader>la", ":<C-U>Lspsaga range_code_action<CR>", desc = "Show code actions", mode = "v" },
+      { "]d", ":Lspsaga diagnostic_jump_next<CR>", desc = "Jump to next diagnostic", mode = "n" },
+      { "[d", ":Lspsaga diagnostic_jump_prev<CR>", desc = "Jump to previous diagnostic", mode = "n" },
+      { "gh", ":Lspsaga hover_doc<CR>", desc = "Show hover doc", mode = "n" },
+      { "gkh", ":Lspsaga hover_doc ++keep<CR>", desc = "Show hover doc [keep]", mode = "n" },
+      { "gr", ":Lspsaga finder<CR>", desc = "Show lsp finder", mode = "n" },
+      { "<leader>lr", ":Lspsaga rename<CR>", desc = "Rename symbol", mode = "n" },
+      { "gl", ":Lspsaga show_line_diagnostics<CR>", desc = "Show line diagnostics", mode = "n" },
+      { "<leader>l.", ":Lspsaga signature_help<CR>", desc = "Show signature help", mode = "n" },
     },
   },
 }
