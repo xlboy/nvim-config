dd20e2b67d3798d8d3655747df5a974b06c841f4
xlboy <xlboy88888@foxmail.com>
2024-01-02 12:27:30 +0800

    1

--- lua/plugins/ui/tabline.lua
+++ lua/plugins/ui/tabline.lua
@@ -1,9 +1,9 @@
 local config = {
   active_color = { bg = "#17c0eb", fg = "#ffffff" },
-  visible_color = { bg = "#2f3640", fg = "#dff9fb" },
+  visible_color = { bg = "#2f3640", fg = "#afc5c7" },
   default_color = { bg = "none", fg = "#B0BEC5" },
   default_surround_char = { left = "", right = "" },
-  min_width = 15,
+  min_width = 20,
 }
 
 local utils = {
@@ -26,6 +26,64 @@ local utils = {
     return vim.tbl_filter(function(bufnr)
       return vim.api.nvim_buf_get_option(bufnr, "buflisted")
     end, vim.api.nvim_list_bufs())
+  end,
+  ---Get the names of all current listed buffers
+  ---@return table
+  get_current_filenames = function()
+    local listed_buffers = vim.tbl_filter(function(bufnr)
+      return vim.bo[bufnr].buflisted and vim.api.nvim_buf_is_loaded(bufnr)
+    end, vim.api.nvim_list_bufs())
+
+    return vim.tbl_map(vim.api.nvim_buf_get_name, listed_buffers)
+  end,
+
+  ---Get unique name for the current buffer
+  ---@param filename string
+  ---@param shorten boolean
+  ---@return string
+  get_unique_filename = function(self, filename, shorten)
+    local filenames = vim.tbl_filter(function(filename_other)
+      return filename_other ~= filename
+    end, self:get_current_filenames())
+
+    if shorten then
+      filename = vim.fn.pathshorten(filename)
+      filenames = vim.tbl_map(vim.fn.pathshorten, filenames)
+    end
+
+    -- Reverse filenames in order to compare their names
+    filename = string.reverse(filename)
+    filenames = vim.tbl_map(string.reverse, filenames)
+
+    local index
+
+    -- For every other filename, compare it with the name of the current file char-by-char to
+    -- find the minimum index `i` where the i-th character is different for the two filenames
+    -- After doing it for every filename, get the maximum value of `i`
+    if next(filenames) then
+      index = math.max(unpack(vim.tbl_map(function(filename_other)
+        for i = 1, #filename do
+          -- Compare i-th character of both names until they aren't equal
+          if filename:sub(i, i) ~= filename_other:sub(i, i) then return i end
+        end
+        return 1
+      end, filenames)))
+    else
+      index = 1
+    end
+
+    -- Iterate backwards (since filename is reversed) until a "/" is found
+    -- in order to show a valid file path
+    while index <= #filename do
+      if filename:sub(index, index) == "/" then
+        index = index - 1
+        break
+      end
+
+      index = index + 1
+    end
+
+    return string.reverse(string.sub(filename, 1, index))
   end,
 }
 
@@ -80,7 +138,9 @@ return {
           end,
           provider = function(self)
             local window_number = utils.get_window_number(self.bufnr)
-            return window_number .. ". "
+            return window_number
+            -- return config.number_chars[window_number]
+            -- return window_number .. ". "
           end,
           hl = function(self)
             local fg = self.is_active and config.active_color.fg or config.visible_color.fg
@@ -90,13 +150,11 @@ return {
         FileNameAndIcon = {
           init = function(self)
             local filename = self.filename
-            self.display_filename = filename == "" and "[No Name]" or vim.fn.fnamemodify(filename, ":t")
+            self.display_filename = filename == "" and "[No Name]" or utils:get_unique_filename(self.filename, true)
             local filename_length = string.len(self.display_filename)
             if filename_length < config.min_width then
               self.display_left_space = math.floor((config.min_width - filename_length) / 2)
               self.display_right_space = config.min_width - filename_length - self.display_left_space
-
-              if not self.is_active and self.is_visible then self.display_left_space = self.display_left_space - 2 end
             end
           end,
           {
@@ -106,10 +164,17 @@ return {
                 return self.display_left_space and self.display_left_space > 0
               end,
               provider = function(self)
-                return string.rep(" ", self.display_left_space)
+                local space_size = self.display_left_space
+                -- 减 1 是因为左边的 surround char
+                if self.is_active or self.is_visible then space_size = space_size - 1 end
+                -- 减 3 是因为左边的 window number（`1. ` ）
+                -- if not self.is_active and self.is_visible then space_size = space_size - 1 end
+                if space_size <= 0 then return "" end
+                return string.rep(" ", space_size)
               end,
             },
             {
+              name = "Icon",
               init = function(self)
                 local filename = self.filename
                 local extension = vim.fn.fnamemodify(filename, ":e")
@@ -124,6 +189,7 @@ return {
               end,
             },
             {
+              name = "FileName",
               provider = function(self)
                 return self.display_filename
               end,
@@ -134,7 +200,13 @@ return {
                 return self.display_right_space and self.display_right_space > 0
               end,
               provider = function(self)
-                return string.rep(" ", self.display_right_space)
+                local space_size = self.display_right_space
+                -- 减 1 是因为右边的 surround char
+                if self.is_active or self.is_visible then space_size = space_size - 1 end
+                if space_size <= 0 then return "" end
+
+                -- if self.is_visible then space_size = space_size - 1 end
+                return string.rep(" ", space_size)
               end,
             },
           },
@@ -184,7 +256,7 @@ return {
         return config.default_color
       end,
       -- components.buffer.ActiveIcon,
-      components.buffer.WindowNumber,
+      -- components.buffer.WindowNumber,
       components.buffer.FileNameAndIcon,
       components.buffer.FileFlags,
     }
